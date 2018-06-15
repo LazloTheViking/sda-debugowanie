7f9638e35e32cb1d39e79677eb45df1b7e796147
Szymon Rybka <artifexet@gmail.com>
2018-06-15 20:54:24 +0200

    Questions are made dynamically from object

--- script.js
+++ script.js
@@ -1,11 +1,90 @@
+const questions = [
+    {
+        label: "Zmienne",
+        questions: [
+            {
+                question: "Czym jest var?",
+                value: 1,
+            },
+            {
+                question: "Czym jest var1?",
+                value: 2,
+            },
+            {
+                question: "Czym jest var2?",
+                value: 3,
+            }
+        ]
+    },
+    {
+        label: "Zmienne2",
+        questions: [
+            {
+                question: "Co jest let?",
+                value: 1,
+            },
+            {
+                question: "Co jest let1?",
+                value: 2,
+            },
+            {
+                question: "Co jest let2?",
+                value: 3,
+            }
+        ]
+    },
+    {
+        label: "Zmienne3",
+        questions: [
+            {
+                question: "Czym jest cons?",
+                value: 1,
+            },
+            {
+                question: "Czym jest cons1?",
+                value: 2,
+            },
+            {
+                question: "Czym jest cons2?",
+                value: 3,
+            }
+        ]
+    },
+];
+
+function createQuestionElement(questionData) {
+    let optionsTemplate = '';
+    questionData.questions.forEach((myOption) => {
+        optionsTemplate += `<option value="${myOption.value}">${myOption.question}</option>\n`;
+    });
+
+    const template = `
+        <div class="row">
+            <div class="input-field col s12">
+                <select multiple>
+                    ${optionsTemplate}
+                </select>
+                <label>${questionData.label}</label>
+            </div>
+        </div>`;
+
+        const element = document.createElement("div");
+        element.innerHTML = template;
+        return element;
+}
+
 let form, modals, elementResult;
 document.addEventListener('DOMContentLoaded', function () {
+    const questionsContainer = document.getElementById('questionsContainer');
+    questions.forEach(questionData => {
+        questionsContainer.appendChild(createQuestionElement(questionData));
+    });
     let elements = document.querySelectorAll('select');
     form = M.FormSelect.init(elements, {});
 
     elements = document.querySelectorAll('.modal');
     modals = M.Modal.init(elements, {});
-    
+
     elementResult = document.getElementById("result");
 });
 
